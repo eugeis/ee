0e293c9533a36e591c961267101318efd3965c0e
Eugen Eisler <eoeisler@gmail.com>
2017-02-27 08:36:08 +0100

    Fix derive

---
+++ ee-lang/src/test/kotlin/ee/lang/gen/kt/GenericDeriveTest.kt
@@ -0,0 +1,34 @@
+package ee.lang.gen.kt
+
+import ee.lang.*
+import ee.lang.gen.prepareForKotlinGeneration
+import org.hamcrest.CoreMatchers
+import org.junit.Assert
+import org.junit.Test
+
+private object TestModel2 : StructureUnit() {
+    object Type1 : CompilationUnit() {
+    }
+
+    object Type2 : CompilationUnit() {
+    }
+
+    object Pojo : CompilationUnit() {
+        val prop1 = prop { type(n.List.GT(Type1)) }
+        val prop2 = prop { type(n.List.GT(Type2)) }
+    }
+}
+
+class GenericDeriveTest {
+
+    @Test
+    fun deriveTest() {
+        TestModel2.prepareForKotlinGeneration()
+
+        val type = TestModel2.Pojo.prop1.type()
+        Assert.assertThat(type.generics()[0].type(), CoreMatchers.sameInstance(TestModel2.Type1 as TypeI))
+
+        val type2 = TestModel2.Pojo.prop2.type()
+        Assert.assertThat(type2.generics()[0].type(), CoreMatchers.sameInstance(TestModel2.Type2 as TypeI))
+    }
+}
