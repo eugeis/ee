14f7170296e5e2adc2fa03e1b8f23f8c1d23cc39
Eugen Eisler <eoeisler@gmail.com>
2018-08-23 22:58:03 +0200

    Update versions

---
+++ ee-design_xsd/src/test/kotlin/ee/design/xsd/XsdToDesignMain.kt
@@ -0,0 +1,44 @@
+package ee.design.swagger
+
+import ee.design.xsd.DslTypes
+import ee.design.xsd.XsdToDesign
+import org.slf4j.LoggerFactory
+import java.nio.file.Paths
+
+object XsdToDesignMain {
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val swagger = XsdToDesign(mutableMapOf(),
+                mutableMapOf("rel.self" to "Link", "Href" to "Link", "AspectId" to "n.String", "ETag" to "n.String",
+                        "Errors" to "n.String", "TenantId" to "n.String", "TypeId" to "n.String"),
+                mutableSetOf("AspectId", "ETag", "Errors", "TenantId", "TypeId"))
+
+        val base = Paths.get("ee/ee-design_swagger/src/test/resources").toRealPath()
+
+        val alreadyGeneratedTypes = mutableMapOf<String, String>()
+        val buffer = StringBuffer()
+        listOf("assetmanagement-v3.yaml").map {
+            swagger.toDslTypes(base.resolve(it))
+        }.forEach { buffer.appendTypes(it, alreadyGeneratedTypes) }
+
+        log.info(buffer.toString())
+    }
+
+    private fun StringBuffer.appendTypes(types: DslTypes, keyToTypes: MutableMap<String, String>): StringBuffer =
+            apply {
+                appendln("//${types.name}")
+                types.types.forEach { k, v ->
+                    if (keyToTypes.containsKey(k)) {
+                        if (keyToTypes[k] != v) {
+                            log.warn("different type definitions in {}, with same name {} first:{} second {}", types.name,
+                                    k, keyToTypes[k], v)
+                        }
+                    } else {
+                        keyToTypes[k] = v
+                        appendln(v)
+                    }
+                }
+            }
+}
