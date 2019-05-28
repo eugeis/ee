87a3b52a720c714f6b720dc889367627f67bd12a
Eugen Eisler <eoeisler@gmail.com>
2019-05-28 15:05:43 +0200

    add class type in template

--- ee-lang/src/main/kotlin/ee/lang/gen/doc/MarkdownPojos.kt
+++ ee-lang/src/main/kotlin/ee/lang/gen/doc/MarkdownPojos.kt
@@ -12,36 +12,48 @@ fun <T : CompilationUnitI<*>> T.toMarkdownClassImpl(c: GenerationContext, derive
                                              nonBlocking: Boolean = isNonBlocking()): String {
     return """
 # ${parent().name()}
+
        ${parent().name()}is a module and artifact mcr-opcua
 
 ## ${namespace()}
+
           ${namespace()} is a package
 ## Properties
-          ${props().joinSurroundIfNotEmptyToString(nL, prefix = nL, postfix = nL) {
-        it.toPlainUmlMember(c, genProps = true) }}
+
+          ${props().joinSurroundIfNotEmptyToString(nL, prefix = nL, postfix = nL) {it.toPlainUmlMember(c, genProps = true) }}
 ## Operations
-          ${operations().joinSurroundIfNotEmptyToString(nL, prefix = nL, postfix = nL)
-    {it.toPlainOperUml(c,genOperations = true)}}
+
+          ${operations().joinSurroundIfNotEmptyToString(nL, prefix = nL, postfix = nL){it.toPlainOperUml(c,genOperations = true)}}
 
 ## PlantUml diagram
     ```plantuml
-    ${toPlainUmlClassImpl(c,startStopUmpl = true)}
+
+        ${toPlainUmlClassImpl(c, startStopUmpl = true)}
     ```
         """
 }
 
 fun <T : CommentI<*>> T.toPlainUml(c: GenerationContext, generateComments: Boolean,startNoteUml:Boolean = true): String =
-        (generateComments && isNotEMPTY()).then {
-            "${startNoteUml(startNoteUml)} ${name()}"
+        (generateComments && isNotEMPTY()).then { "${startNoteUml(startNoteUml)} ${name()}"
         }
 
 fun <T : AttributeI<*>> T.toPlainUmlMember(c: GenerationContext, genProps: Boolean = true): String =
-        (genProps && isNotEMPTY()).then {
-            "${c.n(this)} : ${c.n(type())}"
+        (genProps && isNotEMPTY()).then { "${c.n(this)} : ${c.n(type())}"
         }
 fun OperationI<*>.toPlainOperUml(c: GenerationContext, genOperations:Boolean = true): String = (genOperations && isNotEMPTY()).then {
    // "${it.name()}: ${it.type()}
     "${c.n(this)}"
+}
+
+fun <T : CompilationUnitI<*>> T.toPlainUmlOpenclassImpl(c: GenerationContext, derived: String = LangDerivedKind.IMPL,
+                                                        api: String = LangDerivedKind.API,
+                                                        itemName: String = c.n(this, derived),
+                                                        dataClass: Boolean = this is BasicI<*> &&
+                                                                superUnits().isEmpty() && superUnitFor().isEmpty(),
+                                                        nonBlocking: Boolean = isNonBlocking()): String {
+    return """
+        ${(isOpen() && !dataClass).then("open ")}${dataClass.then("data ")}class $nL ..
+    """
 }
 
 fun <T : CompilationUnitI<*>> T.toPlainUmlClassImpl(c: GenerationContext, derived: String = LangDerivedKind.IMPL,
@@ -52,6 +64,7 @@ fun <T : CompilationUnitI<*>> T.toPlainUmlClassImpl(c: GenerationContext, derive
           ${toPlainUmlTemplateClassImpl(c, startStopUml = false)}
           ${doc().toPlainUml(c, generateComments)}
           }
+
         ${stopUml(startStopUmpl)}"""
 
 }
@@ -64,15 +77,17 @@ fun <T : CompilationUnitI<*>> T.toPlainUmlTemplateClassImpl(c: GenerationContext
             ${startUml(startStopUml)}
                 title ${parent().name()}
                 ${isIfc().ifElse("interface ", "class")} ${name()} {
-                  {field} ${props().joinSurroundIfNotEmptyToString(nL, prefix = nL, postfix = nL) {
-                  it.toPlainUmlMember(c, genProps) }}
-                  {method} ${operations().joinSurroundIfNotEmptyToString(nL, prefix = nL, postfix = nL)
+                  {field}${toPlainUmlOpenclassImpl(c)}
+                  {field} ${props().joinSurroundIfNotEmptyToString(nL, prefix = "", postfix = "") {it.toPlainUmlMember(c, genProps) }}
+                  {method} ${operations().joinSurroundIfNotEmptyToString(nL, prefix = "", postfix = "")
                   {it.toPlainOperUml(c,genOperations)}}
                 }
+
 
             ${stopUml(startStopUml)}
         """
 }
+
 
 private fun stopUml(startStopUml: Boolean) = startStopUml.then("@enduml")
 
