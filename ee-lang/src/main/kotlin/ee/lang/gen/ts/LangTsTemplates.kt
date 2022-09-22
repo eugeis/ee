fa3ffa1e41919ba0ec8b44ae3539238331dcc575
Eugen Eisler <eoeisler@gmail.com>
2022-09-22 18:35:02 +0200

    Fixing some Problem with angular

--- ee-lang/src/main/kotlin/ee/lang/gen/ts/LangTsTemplates.kt
+++ ee-lang/src/main/kotlin/ee/lang/gen/ts/LangTsTemplates.kt
@@ -26,8 +26,8 @@ open class LangTsTemplates {
     open fun <T : CompilationUnitI<*>> moduleComponentSCSS(items: ModuleI<*>, nameBuilder: TemplateI<T>.(T) -> NamesI = defaultNameBuilder) =
         Template("ModuleSCSSComponent", nameBuilder) { item, c -> item.toAngularModuleSCSSComponent(items, c, LangDerivedKind.API) }
 
-    open fun <T : CompilationUnitI<*>> moduleService(items: ModuleI<*>, nameBuilder: TemplateI<T>.(T) -> NamesI = defaultNameBuilder) =
-        Template("ModuleService", nameBuilder) { item, c -> item.toAngularModuleService(items, c, LangDerivedKind.API) }
+    open fun <T : CompilationUnitI<*>> moduleService(items: ModuleI<*>, modules: List<ModuleI<*>>, nameBuilder: TemplateI<T>.(T) -> NamesI = defaultNameBuilder) =
+        Template("ModuleService", nameBuilder) { item, c -> item.toAngularModuleService(items, modules, c, LangDerivedKind.API) }
 
     open fun <T : CompilationUnitI<*>> entityViewComponentTypeScript(items: EntityI<*>, enums: List<EnumTypeI<*>>, basics: List<BasicI<*>>, nameBuilder: TemplateI<T>.(T) -> NamesI = defaultNameBuilder) =
         Template("EntityViewTypeScriptComponent", nameBuilder) { item, c -> item.toAngularEntityViewTSComponent(items, enums, basics, c, LangDerivedKind.API) }
@@ -53,8 +53,8 @@ open class LangTsTemplates {
     open fun <T : CompilationUnitI<*>> basicComponentTypeScript(items: BasicI<*>, nameBuilder: TemplateI<T>.(T) -> NamesI = defaultNameBuilder) =
         Template("BasicTypeScriptComponent", nameBuilder) { item, c -> item.toAngularBasicTSComponent(items, c, LangDerivedKind.API) }
 
-    open fun <T : CompilationUnitI<*>> basicComponentHTML(items: BasicI<*>, nameBuilder: TemplateI<T>.(T) -> NamesI = defaultNameBuilder) =
-        Template("BasicHTMLComponent", nameBuilder) { item, c -> item.toAngularBasicHTMLComponent(items, c, LangDerivedKind.API) }
+    open fun <T : CompilationUnitI<*>> basicComponentHTML(items: BasicI<*>, basics: List<BasicI<*>>, nameBuilder: TemplateI<T>.(T) -> NamesI = defaultNameBuilder) =
+        Template("BasicHTMLComponent", nameBuilder) { item, c -> item.toAngularBasicHTMLComponent(items, basics, c, LangDerivedKind.API) }
 
     open fun <T : CompilationUnitI<*>> basicComponentSCSS(items: BasicI<*>, nameBuilder: TemplateI<T>.(T) -> NamesI = defaultNameBuilder) =
         Template("BasicSCSSComponent", nameBuilder) { item, c -> item.toAngularBasicSCSSComponent(items, c, LangDerivedKind.API) }
